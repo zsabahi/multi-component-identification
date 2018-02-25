ef2833bc091c0d4828730fa8ad47dedea780698c
zsabahi <36479223+zsabahi@users.noreply.github.com>
2018-02-25 15:38:59 +0000

    Delete Config.java

--- src/automataChecker/Config.java
+++
@@ -1,7 +0,0 @@
-package automataChecker;
-
-public class Config {
-	static boolean checkActionNames = true;
-	static boolean checkPacketLengths = false;
-	static boolean checkFlowParameters = true;
-}
